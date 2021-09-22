fbfb24f52f4312ed4e68a1ed2b9702889b49ac5f
mg-kh <tragonn18@gmail.com>
2021-09-22 23:02:28 +0630

    add svg

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:weather/constants.dart';
 import 'package:weather/routes.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,69 +15,98 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       getPages: routes,
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Nunito'
-      ),
-      home: Scaffold(
-        backgroundColor: kPrimaryColor,
-        body: Container(
-          padding: EdgeInsets.only(left: 40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
+      theme: ThemeData(fontFamily: 'Nunito'),
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: kPrimaryColor,
+          body: Stack(
             children: [
-              Text(
-                'Explore',
-                style: kHomeScreenTextStyle,
+              Container(
+                padding: EdgeInsets.only(left: 40),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Explore',
+                      style: kHomeScreenTextStyle,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      'Weather',
+                      style: kHomeScreenTextStyle,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      'Data',
+                      style: kHomeScreenTextStyle,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      'Explore weather conditions\n in other countries.',
+                      style: kHomeScreenDesTextStyle,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    //Button
+                    SizedBox(
+                      width: 130,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Get.toNamed('/map');
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Explore'),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Icon(
+                              Icons.arrow_right_alt,
+                              size: 20,
+                            ),
+                          ],
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            primary: kSecondaryColor,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30))),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                'Weather',
-                style: kHomeScreenTextStyle,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                'Data',
-                style: kHomeScreenTextStyle,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                'Explore weather conditions\n in other countries.',
-                style: kHomeScreenDesTextStyle,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              //Button
-              SizedBox(
-                width: 130,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Get.toNamed('/map');
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Explore'),
-                      SizedBox(width: 10,),
-                      Icon(Icons.arrow_right_alt, size: 20,),
-                    ],
+
+              //! Top svg
+              Positioned(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    child: SvgPicture.asset('assets/images/top.svg'),
                   ),
-                  style: ElevatedButton.styleFrom(
-                      elevation: 0,
-                      primary: kSecondaryColor,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30))),
+                ),
+              ),
+
+              //! Bottom svg
+              Positioned(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                    child: SvgPicture.asset('assets/images/bottom.svg'),
+                  ),
                 ),
               ),
             ],
